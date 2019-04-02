613151895c07c7ff6babc096f52d21b40b0d106c
Joshua Tucker <thejltucker@gmail.com>
2019-04-02 15:40:43 -0400

    Add project files.

---
+++ PlaygroundApps/ProgramUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PlaygroundApps
+{
+    public class ProgramUI
+    {
+        public ProgramUI()
+        {
+        }
+
+        public void Run()
+        {
+            while(Menu()) { }
+        }
+
+        private bool Menu()
+        {
+            Console.WriteLine("Cats > Birds");
+            return true;
+        }
+    }
+}
