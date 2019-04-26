31298f7a056d94385087c2fbcb49a43aea5142ac
Joshua Tucker <thejltucker@gmail.com>
2019-04-25 21:32:23 -0400

    Added Calculation Class

---
+++ Calculator_Repository/Calculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Repository
+{
+    public class Calculation
+    {
+        public List<string> Constants { get; set; }
+        public List<char> Operators { get; set; }
+
+        public Calculation()
+        {
+            Constants = new List<string>();
+            Operators = new List<char>();
+        }
+    }
+}
